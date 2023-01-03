e49077523603194d66f131052916c176d37c157b
Derreck <derreck775@gmail.com>
2023-01-03 09:54:08 -0300

    blog

--- db.py
+++ db.py
@@ -1,41 +1,21 @@
 import sqlite3
 
-import click
-from flask import current_app, g
+connection = sqlite3.connect('database.db')
 
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+with open('schema.sql') as f:
+    connection.executescript(f.read())
 
-    return g.db
+cur = connection.cursor()
 
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content for the first post')
+            )
 
-def close_db(e=None):
-    db = g.pop('db', None)
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second Post', 'Content for the second post')
+            )
 
-    if db is not None:
-        db.close()
-
-def init_db():
-    db = get_db()
-
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-
-
-
-@click.command('init-db')
-def init_db_command():
-    """Clear the existing data and create new tables."""
-    init_db()
-    click.echo('Initialized the database.')
-
-def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
+connection.commit()
+connection.close()
 
